ec43d1ced8a3614dd58ebd25f858a066f26507f9
dam-ian <63857986+dam-ian@users.noreply.github.com>
2020-04-26 19:08:04 +0200

    Add files via upload

---
+++ src/song.kt
@@ -0,0 +1,24 @@
+/*
+Description of my favorite song "My I have this dance"
+ */
+
+fun main(args: Array<String>) { // class to pass arguments
+
+    val song = "May I Have This Dance"
+    val version = "remix"
+    val artist = "Francis and the Lights"
+    val featArtist = "Chance the Rapper"
+    val album = "Farewell, Starlite!"
+    val released = 2017
+    val genre = "Pop"
+    val durationInSec = 195
+
+    println("$song") //println statement prints everything inside and appends new line
+    println("$version") //$ used tp print variable inside ""
+    println("$artist")
+    println("$featArtist")
+    println("$album")
+    println("$released")
+    println("$genre")
+    println("$durationInSec")
+}
